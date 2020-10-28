71f8c5cbecfbe2349b36d88b02e116b410cde1c4
Fernando Chavez <fjchavez01@gmail.com>
2020-10-28 13:37:29 -0700

    mvp done

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,7 @@
 import { useLocalStorage } from './useLocalStorage'
+import {} from 'react'
 
-export const useDarkMode = () => {
-    
+export default function useDarkMode(enabled){
+    const [value, setValue] = useLocalStorage(enabled)
+    return[value,setValue]
 }
